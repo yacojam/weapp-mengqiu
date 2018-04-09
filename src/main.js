9ed740ddb9ef819029807a58a269fd5d66fd2491
vien-li <33721862+vien-li@users.noreply.github.com>
2018-04-09 10:50:21 +0800

    主页
    主页图片以及tabbar配置

--- src/main.js
+++ src/main.js
@@ -11,12 +11,30 @@ export default {
   // 这个字段走 app.json
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
-    pages: ['pages/logs/main', '^pages/index/main'],
+    pages: ['pages/logs/main', '^pages/index/main', 'pages/release/main'],
     window: {
       backgroundTextStyle: 'light',
-      navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'WeChat',
+      navigationBarBackgroundColor: '#FFE955',
+      navigationBarTitleText: '萌球社区',
       navigationBarTextStyle: 'black'
+    },
+    tabBar: {
+      list: [{
+        pagePath: 'pages/index/main',
+        text: '萌球社区',
+        iconPath: './static/imgs/index/icon_home_nor@3x.png',
+        selectedIconPath: './static/imgs/index/icon_home_sel@3x.png'
+      }, {
+        pagePath: 'pages/release/main',
+        text: '发布',
+        iconPath: './static/imgs/index/icon_send@3x.png',
+        selectedIconPath: './static/imgs/index/icon_send@3x.png'
+      }, {
+        pagePath: 'pages/logs/main',
+        text: '铲屎官',
+        iconPath: './static/imgs/index/icon_user_nor@3x.png',
+        selectedIconPath: './static/imgs/index/icon_user_sel@3x.png'
+      }]
     }
   }
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,105 +1,168 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
+  <div class="container">
 
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
+    <div class="header-bar">
+      <div class="for-recommend" :class="{ active: showRecommend}" @click="changeContent('recommend')">推荐</div>
+      <div class="for-attention" :class="{ active: !showRecommend}" @click="changeContent('attention')">关注</div>
+    </div>
+
+    <div class="recommend">
+      <div class="user-shows-content">
+        <div class="head-portrait left-correction">
+          <img src="../../../static/imgs/index/feed_icon_photo@3x.png" alt="用户头像">
+        </div>
+        <div class="info">
+          <p class="name">Sabar</p>
+          <p class="time"><span class="subtime">昨天</span>09:32</p>
+        </div>
+
+        <p class="title left-correction">这种天气最适合带着丫丫出去遛弯儿啦~~</p>
+
+        <div class="big-show">
+          <img src="../../../static/imgs/index/0.jpg" alt="展示用大图" mode="scaleToFill" width="100%">
+        </div>
+
+        <div class="operate left-correction">
+          <span class="like">
+            <img src="../../../static/imgs/index/feed_icon_collect_nor@2x.png" alt="like">
+          </span>
+          <span class="forward">
+            <img src="../../../static/imgs/index/feed_icon_share@2x.png" alt="like">
+          </span>
+          <span class="give-like">
+            <img src="../../../static/imgs/index/feed_icon_like_nor@2x.png" alt="like">
+          </span>
+          <span class="likes-counts">36</span>
+          <span class="comments">
+            <img src="../../../static/imgs/index/feed_icon_comment@2x.png" alt="like">
+          </span>
+          <span class="comments-counts">11</span>
+        </div>
+
+        <div class="bottom-line"></div>
       </div>
     </div>
 
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
+    <div class="attention"></div>
 
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
-
 export default {
   data () {
     return {
-      motto: 'Hello World',
-      userInfo: {}
+      showRecommend: true
     }
   },
 
-  components: {
-    card
-  },
+  components: {},
 
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    changeContent (type) {
+      (type === 'recommend') ? this.showRecommend = true : this.showRecommend = false
     }
   },
 
-  created () {
-    // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
-  }
+  created () {}
 }
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+.left-correction {
+  padding-left: 40rpx;
+}
+.header-bar {
+  width: 100%;
+  background-color: #ffe955;
+}
+.active {
+  border-bottom: 8rpx solid #000;
 }
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
+.for-recommend,
+.for-attention {
+  display: inline-block;
+  padding: 25rpx 2rpx;
+  font-size: 30rpx;
+  color: #000;
+  margin-left: 50rpx;
+}
+
+.head-portrait img {
+  width: 66rpx;
+  height: 66rpx;
   border-radius: 50%;
 }
 
-.userinfo-nickname {
-  color: #aaa;
+.recommend {
+  width: 100%;
 }
-
-.usermotto {
-  margin-top: 150px;
+.user-shows-content {
+  padding: 40rpx 0 0;
 }
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
+.info,
+.head-portrait {
   display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+  height: 60rpx;
+  margin-bottom: 50rpx;
+}
+.info {
+  vertical-align: bottom;
+  margin-left: 26rpx;
+}
+.info .name {
+  font-family: "HelveticaNeueLTPro Md";
+  font-size: 26rpx;
+  color: #000;
+  margin-bottom: 14rpx;
+}
+.info .time {
+  font-family: "HelveticaNeueLTPro Lt";
+  font-size: 22rpx;
+  color: #000;
+  opacity: 0.7;
+}
+.time .subtime {
+  margin-right: 12rpx;
+}
+.recommend .title {
+  font-size: 32rpx;
+  font-family: "PingFang SC";
+  margin-bottom: 30rpx;
+}
+.recommend .big-show img {
+  width: 100%;
+  height: 500rpx;
+}
+.operate {
+  display: flex;
+  align-items: center;
+  padding-top: 30rpx; 
+  padding-bottom: 30rpx; 
+}
+.operate img {
+  width: 46rpx;
+  height: 46rpx;
+}
+.operate .likes-counts,
+.operate .comments-counts {
+  font-family: "HelveticaNeueLTPro Roman";
+  font-size: 32rpx;
+  color: #000;
+  display: inline-block;
+  width: 80rpx;
+}
+.operate .like, .operate .forward {
+  margin-right: 46rpx;
+}
+.operate .give-like, .operate .comments {
+  margin-right: 16rpx;
+} 
+
+.bottom-line {
+  width: 100%;
+  background-color: #f1f1fa;
+  height: 18rpx;
 }
 </style>
